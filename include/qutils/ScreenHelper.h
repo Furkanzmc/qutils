65755a9ed6400b4f2f47327534709cfad8a1444b
Furkan Üzümcü <furkanuzumcu@gmail.com>
2017-04-20 14:57:44 +0300

    Add DPI support

--- include/qutils/ScreenHelper.h
+++ include/qutils/ScreenHelper.h
@@ -6,6 +6,7 @@ namespace zmc
 
 /**
  * @brief ScreenHelper is a utility class to help create resolution indepentant UI in QML.
+ * When using different icons for different screen densities.
  * @code
  *     // in main.cpp
  *     ScreenHelper manager;
@@ -28,12 +29,72 @@ class ScreenHelper : public QObject
     Q_OBJECT
 
 public:
+    Q_PROPERTY(bool ldpi READ isLDPI)
+    Q_PROPERTY(bool mdpi READ isMDPI)
+    Q_PROPERTY(bool hdpi READ isHDPI)
+
+    Q_PROPERTY(bool xhdpi READ isXHDPI)
+    Q_PROPERTY(bool xxhdpi READ isXXHDPI)
+    Q_PROPERTY(bool xxxhdpi READ isXXXHDPI)
+
+public:
     explicit ScreenHelper(QObject *parent = 0);
 
     Q_INVOKABLE qreal dp(const qreal &size);
 
+    Q_INVOKABLE QString getLowResourceFolderName() const;
+    void setLowResourceFolderName(const QString &resourceName);
+
+    Q_INVOKABLE QString getMediumResourceFolderName() const;
+    void setMediumResourceFolderName(const QString &resourceName);
+
+    Q_INVOKABLE QString getHighResourceFolderName() const;
+    void setHighResourceFolderName(const QString &resourceName);
+
+    Q_INVOKABLE QString getXHighResourceFolderName() const;
+    void setXHighResourceFolderName(const QString &resourceName);
+
+    Q_INVOKABLE QString getXXHighResourceFolderName() const;
+    void setXXHighResourceFolderName(const QString &resourceName);
+
+    Q_INVOKABLE QString getXXXHighResourceFolderName() const;
+    void setXXXHighResourceFolderName(const QString &resourceName);
+
+    Q_INVOKABLE QString getResourceFolderName() const;
+
+    /**
+     * @brief Returns the resource in the current DPI path.
+     * @param fileName
+     * @return Resrouce path
+     */
+    Q_INVOKABLE QString getResource(const QString &fileName) const;
+
 private:
     const qreal m_DPI;
+    const unsigned int m_LowDPIValue,
+          m_MediumDPIValue,
+          m_HighDPIValue,
+          m_XHighDPIValue,
+          m_XXHighDPIValue,
+          m_XXXHighDPIValue,
+          m_DPIVariation;
+
+    // You can use these values to automatically append or prepend to your resource name based on the DPI value.
+    QString m_LowResourceName,
+            m_MediumResourceName,
+            m_HighResourceName,
+            m_XHighResourceName,
+            m_XXHighResourceName,
+            m_XXXHighResourceName;
+
+private:
+    bool isLDPI() const;
+    bool isMDPI() const;
+    bool isHDPI() const;
+
+    bool isXHDPI() const;
+    bool isXXHDPI() const;
+    bool isXXXHDPI() const;
 
 };
 
--- src/ScreenHelper.cpp
+++ src/ScreenHelper.cpp
@@ -2,6 +2,9 @@
 // Qt
 #include <QGuiApplication>
 #include <QScreen>
+#ifdef QT_DEBUG
+#include <QDebug>
+#endif // QT_DEBUG
 // zmc
 #include "qutils/Macros.h"
 
@@ -11,8 +14,17 @@ namespace zmc
 ScreenHelper::ScreenHelper(QObject *parent)
     : QObject(parent)
     , m_DPI(QGuiApplication::primaryScreen()->physicalDotsPerInch())
+    , m_LowDPIValue(120)
+    , m_MediumDPIValue(160)
+    , m_HighDPIValue(240)
+    , m_XHighDPIValue(320)
+    , m_XXHighDPIValue(480)
+    , m_XXXHighDPIValue(640)
+    , m_DPIVariation(20)
 {
-
+#ifdef QT_DEBUG
+    qDebug() << "The DPI is: " << m_DPI;
+#endif // QT_DEBUG
 }
 
 qreal ScreenHelper::dp(const qreal &size)
@@ -27,6 +39,124 @@ qreal ScreenHelper::dp(const qreal &size)
 #endif // Platform Check
 
     return newSize;
+}
+
+QString ScreenHelper::getLowResourceFolderName() const
+{
+    return m_LowResourceName;
+}
+
+void ScreenHelper::setLowResourceFolderName(const QString &resourceName)
+{
+    m_LowResourceName = resourceName;
+}
+
+QString ScreenHelper::getMediumResourceFolderName() const
+{
+    return m_MediumResourceName;
+}
+
+void ScreenHelper::setMediumResourceFolderName(const QString &resourceName)
+{
+    m_MediumResourceName = resourceName;
+}
+
+QString ScreenHelper::getHighResourceFolderName() const
+{
+    return m_HighResourceName;
+}
+
+void ScreenHelper::setHighResourceFolderName(const QString &resourceName)
+{
+    m_HighResourceName = resourceName;
+}
+
+QString ScreenHelper::getXHighResourceFolderName() const
+{
+    return m_XHighResourceName;
+}
+
+void ScreenHelper::setXHighResourceFolderName(const QString &resourceName)
+{
+    m_XHighResourceName = resourceName;
+}
+
+QString ScreenHelper::getXXHighResourceFolderName() const
+{
+    return m_XXHighResourceName;
+}
+
+void ScreenHelper::setXXHighResourceFolderName(const QString &resourceName)
+{
+    m_XXHighResourceName = resourceName;
+}
+
+QString ScreenHelper::getXXXHighResourceFolderName() const
+{
+    return m_XXXHighResourceName;
+}
+
+void ScreenHelper::setXXXHighResourceFolderName(const QString &resourceName)
+{
+    m_XXXHighResourceName = resourceName;
+}
+
+QString ScreenHelper::getResourceFolderName() const
+{
+    QString name = getLowResourceFolderName();
+
+    if (isMDPI()) {
+        name = getMediumResourceFolderName();
+    }
+    else if (isHDPI()) {
+        name = getHighResourceFolderName();
+    }
+    else if (isXHDPI()) {
+        name = getXHighResourceFolderName();
+    }
+    else if (isXXHDPI()) {
+        name = getXXHighResourceFolderName();
+    }
+    else if (isXXXHDPI()) {
+        name = getXXXHighResourceFolderName();
+    }
+
+    return name;
+}
+
+QString ScreenHelper::getResource(const QString &fileName) const
+{
+    return getResourceFolderName() + "/" + fileName;
+}
+
+bool ScreenHelper::isLDPI() const
+{
+    return m_DPI < m_LowDPIValue + m_DPIVariation;
+}
+
+bool ScreenHelper::isMDPI() const
+{
+    return m_DPI < m_MediumDPIValue + m_DPIVariation && m_DPI > m_LowDPIValue + m_DPIVariation;
+}
+
+bool ScreenHelper::isHDPI() const
+{
+    return m_DPI < m_HighDPIValue + m_DPIVariation && m_DPI > m_MediumDPIValue + m_DPIVariation;
+}
+
+bool ScreenHelper::isXHDPI() const
+{
+    return m_DPI < m_XHighDPIValue + m_DPIVariation && m_DPI > m_HighDPIValue + m_DPIVariation;
+}
+
+bool ScreenHelper::isXXHDPI() const
+{
+    return m_DPI < m_XXHighDPIValue + m_DPIVariation && m_DPI > m_XHighDPIValue + m_DPIVariation;
+}
+
+bool ScreenHelper::isXXXHDPI() const
+{
+    return m_DPI < m_XXXHighDPIValue + m_DPIVariation && m_DPI > m_XXHighDPIValue + m_DPIVariation;
 }
 
 }
