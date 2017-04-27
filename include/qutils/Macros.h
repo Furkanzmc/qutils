fc6716a0a42f5d341d6f5fce5317cfb20dd3858d
Furkan Üzümcü <furkanuzumcu@gmail.com>
2017-04-27 16:08:22 +0300

    Add log macros

--- include/qutils/Macros.h
+++ include/qutils/Macros.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <QDebug>
 #include <qsystemdetection.h>
 
 #if defined(Q_OS_WIN) || defined(Q_OS_MAC) || (defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID))
@@ -8,3 +9,13 @@
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
 #define Q_OS_MOBILE
 #endif // Desktop Platform
+
+#ifdef Q_OS_MOBILE
+#define LOG(msg) qDebug() << "[INFO] " << msg
+#define LOG_WARNING(msg) qDebug() << "[WARNING] " << msg
+#define LOG_ERROR(msg) qDebug() << "[ERROR] " << msg
+#else
+#define LOG(msg) qDebug() << "[INFO]" << __FUNCTION__ << "(" << __LINE__ << "):" << msg
+#define LOG_WARNING(msg) qDebug() << "[WARNING]" << __FUNCTION__ << ":" << __LINE__ << "):" << msg
+#define LOG_ERROR(msg) qDebug() << "[ERROR]" << __FUNCTION__ << "(" << __LINE__ << "):" << msg
+#endif // Q_OS_MOBILE
