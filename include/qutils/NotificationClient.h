0b4c413883dcdae7a413bdd2f2c12a17dd7763a7
Furkan Üzümcü <furkanuzumcu@gmail.com>
2017-08-01 18:43:36 -0400

    Add an empty NotificationClient class for platforms that do not supported yet so that the QML code won't raise an error

--- include/qutils/NotificationClient.h
+++ include/qutils/NotificationClient.h
@@ -4,3 +4,31 @@
 #ifdef Q_OS_ANDROID
 #include "android/NotificationClient_Android.h"
 #endif // Q_OS_ANDROID
+
+#if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
+namespace zmc
+{
+
+class NotificationClient : public QObject
+{
+    Q_OBJECT
+
+public:
+    NotificationClient()
+    {
+
+    }
+
+    Q_INVOKABLE void schedule(QVariantMap data)
+    {
+        Q_UNUSED(data);
+    }
+
+signals:
+    void notificationReceived(const QString &payload);
+    void notificationTapped(const QString &payload);
+    void fcmTokenReceived(const QString &token);
+};
+
+}
+#endif // !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
