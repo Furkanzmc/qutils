95515508bf8ff2120e55f09d287bf0b6c207ae5e
Furkan Üzümcü <furkanuzumcu@gmail.com>
2017-08-24 22:20:57 -0400

    Use the placeholder NotificationClient for iOS until a proper one is implemented

--- include/qutils/NotificationClient.h
+++ include/qutils/NotificationClient.h
@@ -5,7 +5,7 @@
 #include "android/NotificationClient_Android.h"
 #endif // Q_OS_ANDROID
 
-#if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
+#if !defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
 namespace zmc
 {
 
