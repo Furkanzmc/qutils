37627f903ee4bdae60d800f3e3cdc58712645731
Furkan Üzümcü <furkanuzumcu@gmail.com>
2017-09-17 13:59:29 -0400

    Convert it to a class for better code completion support

--- include/qutils/CppTypes.h
+++ include/qutils/CppTypes.h
@@ -5,58 +5,71 @@
 namespace zmc
 {
 
+#pragma once
+#include <QObject>
+
 /**
- * @brief This namespace will contain some useful enums that are not directly exposed to QML by Qt.
+ * @brief This will contain some useful enums that are not directly exposed to QML by Qt.
  */
-namespace CppTypes
+class CppTypes : public QObject
 {
+    Q_OBJECT
 
-Q_NAMESPACE
+public:
+    // The table is copied from here: http://doc.qt.io/qt-5/qnetworkreply.html#NetworkError-enum
+    enum NetworkError : unsigned int {
+        NoError = 0,
+        ConnectionRefusedError = 1,
+        RemoteHostClosedError = 2,
+        HostNotFoundError = 3,
+        TimeoutError = 4,
+        OperationCanceledError = 5,
+        SslHandshakeFailedError = 6,
+        TemporaryNetworkFailureError = 7,
+        NetworkSessionFailedError = 8,
+        BackgroundRequestNotAllowedError = 9,
+        TooManyRedirectsError = 10,
+        InsecureRedirectError = 11,
+        ProxyConnectionRefusedError = 101,
+        ProxyConnectionClosedError = 102,
+        ProxyNotFoundError = 103,
+        ProxyTimeoutError = 104,
+        ProxyAuthenticationRequiredError = 105,
+        ContentAccessDenied = 201,
+        ContentOperationNotPermittedError = 202,
+        ContentNotFoundError = 203,
+        AuthenticationRequiredError = 204,
+        ContentReSendError = 205,
+        ContentConflictError = 206,
+        ContentGoneError = 207,
+        InternalServerError = 401,
+        OperationNotImplementedError = 402,
+        ServiceUnavailableError = 403,
+        ProtocolUnknownError = 301,
+        ProtocolInvalidOperationError = 302,
+        UnknownNetworkError = 99,
+        UnknownProxyError = 199,
+        UnknownContentError = 299,
+        ProtocolFailure = 399,
+        UnknownServerError = 499
+    };
+    Q_ENUM(NetworkError)
 
-// The table is copied from here: http://doc.qt.io/qt-5/qnetworkreply.html#NetworkError-enum
-enum NetworkError : unsigned int {
-    NoError = 0,
-    ConnectionRefusedError = 1,
-    RemoteHostClosedError = 2,
-    HostNotFoundError = 3,
-    TimeoutError = 4,
-    OperationCanceledError = 5,
-    SslHandshakeFailedError = 6,
-    TemporaryNetworkFailureError = 7,
-    NetworkSessionFailedError = 8,
-    BackgroundRequestNotAllowedError = 9,
-    TooManyRedirectsError = 10,
-    InsecureRedirectError = 11,
-    ProxyConnectionRefusedError = 101,
-    ProxyConnectionClosedError = 102,
-    ProxyNotFoundError = 103,
-    ProxyTimeoutError = 104,
-    ProxyAuthenticationRequiredError = 105,
-    ContentAccessDenied = 201,
-    ContentOperationNotPermittedError = 202,
-    ContentNotFoundError = 203,
-    AuthenticationRequiredError = 204,
-    ContentReSendError = 205,
-    ContentConflictError = 206,
-    ContentGoneError = 207,
-    InternalServerError = 401,
-    OperationNotImplementedError = 402,
-    ServiceUnavailableError = 403,
-    ProtocolUnknownError = 301,
-    ProtocolInvalidOperationError = 302,
-    UnknownNetworkError = 99,
-    UnknownProxyError = 199,
-    UnknownContentError = 299,
-    ProtocolFailure = 399,
-    UnknownServerError = 499
+public:
+    explicit CppTypes(QObject *parent = nullptr)
+        : QObject(parent)
+    {
+
+    }
+
+    static void registerType()
+    {
+        qmlRegisterUncreatableType<CppTypes>("qutils.CppTypes", 1, 0, "CppTypes", "Error: only enums");
+    }
+
+signals:
+
+public slots:
 };
-Q_ENUM_NS(NetworkError)
-
-static void registerType()
-{
-    qmlRegisterUncreatableMetaObject(CppTypes::staticMetaObject, "qutils.CppTypes", 1, 0, "CppTypes", "Error: only enums");
-}
-
-}
 
 }
