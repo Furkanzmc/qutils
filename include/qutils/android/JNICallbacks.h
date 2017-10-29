088348395901e30d8d3c53561c9a34282387dbab
Furkan Üzümcü <furkanuzumcu@gmail.com>
2017-10-29 14:03:24 -0400

    Reformat file

--- include/qutils/android/JNICallbacks.h
+++ include/qutils/android/JNICallbacks.h
@@ -10,19 +10,19 @@ public:
     static void notificationReceivedCallback(JNIEnv */*env*/, jobject /*obj*/, jstring jtag, jint id, jstring jnotificationManagerName, jstring notificationPayload);
     static void notificationTappedCallback(JNIEnv */*env*/, jobject /*obj*/, jstring jtag, jint id, jstring jnotificationManagerName, jstring notificationPayload);
     static void backButtonPressedCallback(JNIEnv */*env*/, jobject /*obj*/);
-    static void menuButtonPressedCallback(JNIEnv */*env*/, jobject /*obj*/);
 
+    static void menuButtonPressedCallback(JNIEnv */*env*/, jobject /*obj*/);
     static void alertDialogClickedCallback(JNIEnv */*env*/, jobject /*obj*/, jint buttonIndex);
     static void datePickedCallback(JNIEnv */*env*/, jobject /*obj*/, jint year, jint month, jint day);
-    static void timePickedCallback(JNIEnv */*env*/, jobject /*obj*/, jint hourOfDay, jint minute);
 
+    static void timePickedCallback(JNIEnv */*env*/, jobject /*obj*/, jint hourOfDay, jint minute);
     static void cameraCapturedCallback(JNIEnv */*env*/, jobject /*obj*/, jstring capturePath);
     static void fileSelectedCallback(JNIEnv */*env*/, jobject /*obj*/, jstring filePath);
-    static void cameraCaptureCancelledCallback(JNIEnv */*env*/, jobject /*obj*/);
 
+    static void cameraCaptureCancelledCallback(JNIEnv */*env*/, jobject /*obj*/);
     static void fileSelectionCancelledCallback(JNIEnv */*env*/, jobject /*obj*/);
     static void keyboardHeightChangedCallback(JNIEnv */*env*/, jobject /*obj*/, jint keyboardHeight);
-    static void fcmTokenReceived(JNIEnv */*env*/, jobject /*obj*/, jstring jniToken);
 
+    static void fcmTokenReceived(JNIEnv */*env*/, jobject /*obj*/, jstring jniToken);
     static void openedWithURL(JNIEnv */*env*/, jobject /*obj*/, jstring jniURL);
 };
