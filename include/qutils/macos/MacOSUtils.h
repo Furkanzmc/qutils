9e32b3b9ab3d2099b1ff909fea7f7187c5bacbc1
Furkan Üzümcü <furkanuzumcu@gmail.com>
2018-07-01 14:16:07 -0400

    Convert comments to QDoc style documentation

--- include/qutils/macos/MacOSUtils.h
+++ include/qutils/macos/MacOSUtils.h
@@ -16,64 +16,72 @@ public:
 
     bool eventFilter(QObject *obj, QEvent *event);
 
-    /**
-     * @brief Returns true if this is the main controller.
-     * @return bool
+    /*!
+     * \brief Returns true if this is the main controller.
+     * \return bool
      */
     Q_INVOKABLE bool isMainController() const;
 
-    /**
-     * @brief Set the main controller to this one. When an instance is set as main controller, certain signals will only be emitted for this instance.
+    /*!
+     * \brief Set the main controller to this one.
+     * \param isMain
+     *
+     * When an instance is set as main controller, certain signals will only be emitted for this instance.
      * Only one instance can be the main controller. When an instance is set as main controller while there is already an existing main controller, the existing
      * main controller will relinquish its position and the new instance will be the new main controller.
      * The signals that are designated to main controller are:
-     * - openedWithUrl
-     * - openedWithoutUrl
-     * @param isMain
-     * @return void
+     * \list
+     *     \li openedWithUrl
+     *     \li openedWithoutUrl
+     * \endlist
      */
     Q_INVOKABLE void setMainController(bool isMain, bool disableOthers = true);
 
-    /**
-     * @brief Only the first instance will be notified of this.
-     * @param url
+    /*!
+     * \brief Only the first instance will be notified of this.
+     * \param url
      */
     static void emitOpenedWithURLSignal(QString url);
 
-    /**
-     * @brief Only the first instance will be notified of this.
-     * @param url
+    /*!
+     * \brief Only the first instance will be notified of this.
+     * \param url
      */
     static void emitOpenedWithoutURLSignal();
 
 signals:
-    /**
-     * @brief This signal is emitted when the app is opened with a URL.
+    /*!
+     * \brief This signal is emitted when the app is opened with a URL.
+     * \param url
+     *
      * Follow this tutorial for how to implement int into your app: https://developer.android.com/training/app-links/index.html
-     * @param url
      */
     void openedWithURL(QString url);
 
-    /**
-     * @brief This is also called for the first instance. This is just a complementary signal. This is emitted If an app is not opened from a URL.
+    /*!
+     * \brief This is also called for the first instance.
+     *
+     * This is just a complementary signal. This is emitted If an app is not opened from a URL.
      */
     void openedWithoutURL();
 
-    /**
-     * @brief Emitted when m_IsMainController is changed.
-     * @return void
+    /*!
+     * \brief Emitted when m_IsMainController is changed.
+     * \return void
      */
     void mainControllerChanged();
 
 private:
 
     /*!
+     * \internal
      * \variable static QMap<int, CacheManager *> m_Instances
      * \brief This variable is used to keep track of all the instances so that we can send signals to all of them.
      */
     static QMap<int, MacOSUtils *> m_Instances;
 
     /*!
+     * \internal
      * \variable const int m_InstanceID
      * \brief This variable is initilized in the constructor to the size value of m_Instances.
      *
