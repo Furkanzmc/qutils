227aa0fb31d9d71c611a65469389336dda0874a9
Furkan Üzümcü <furkanuzumcu@gmail.com>
2018-08-17 12:37:56 -0400

    Reformat

--- src/SignalManager.cpp
+++ src/SignalManager.cpp
@@ -28,11 +28,9 @@ void SignalManager::emitSignal(const QString &signalName, const QString &targetO
             if (targetObjectName.isEmpty()) {
                 QMetaObject::invokeMethod(instance, std::bind(&SignalManager::signalReceived, instance, signalName, data), Qt::QueuedConnection);
             }
-            else {
-                if (targetObjectName == instance->objectName()) {
-                    QMetaObject::invokeMethod(instance, std::bind(&SignalManager::signalReceived, instance, signalName, data), Qt::QueuedConnection);
-                    break;
-                }
+            else if (targetObjectName == instance->objectName()) {
+                QMetaObject::invokeMethod(instance, std::bind(&SignalManager::signalReceived, instance, signalName, data), Qt::QueuedConnection);
+                break;
             }
         }
     }
